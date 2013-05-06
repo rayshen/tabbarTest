dbc3dc2f126bd6ac458027181cf22894c3912026
Kevin Lee <Kevin@riomoutekiMac.local>
2013-05-06 15:25:20 +0800

    Initial Commit

---
+++ tabbarTest/tabbarViewController.m
@@ -0,0 +1,29 @@
+//
+//  tabbarViewController.m
+//  tabbarTest
+//
+//  Created by Kevin Lee on 13-5-6.
+//  Copyright (c) 2013年 Kevin. All rights reserved.
+//
+
+#import "tabbarViewController.h"
+
+@interface tabbarViewController ()
+
+@end
+
+@implementation tabbarViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
