8ddf3b817a6905da4dbe6e19e0c690b69a0743fd
rayshen <rayshen1012@gmail.com>
2015-08-01 20:21:58 +0800

    1
    1

--- tabbarTest/tabbarViewController.h
+++ tabbarTest/tabbarViewController.h
@@ -8,6 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
-@interface tabbarViewController : UIViewController
+#import "FirstViewController.h"
+#import "SecondViewController.h"
+
+#define iPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : 0)
+#define addHeight 88
+
+
+@protocol tabbarDelegate <NSObject>
+
+-(void)touchBtnAtIndex:(NSInteger)index;
 
 @end
+
+@class tabbarView;
+
+@interface tabbarViewController : UIViewController<tabbarDelegate>
+
+@property(nonatomic,strong) tabbarView *tabbar;
+@property(nonatomic,strong) NSArray *arrayViewcontrollers;
+@end
+
+
+
